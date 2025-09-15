34d5d3a9e1c610fc2d7c0679071643e3ae473890
Edison Andres Perez Martinez <edisonandresperezmartinez@gmail.com>
2025-09-15 10:21:20 -0500

    console de prueba para saber como se renderiza las pantallas

---
+++ src/app/(shop)/checkout/layout.tsx
@@ -0,0 +1,16 @@
+
+
+
+
+export default function CheckoutLayout({
+  children
+}: {
+  children: React.ReactNode;
+}){
+  console.log("holaaaaaaa")
+  return (
+    <>
+      {children}
+    </>
+  )
+}
