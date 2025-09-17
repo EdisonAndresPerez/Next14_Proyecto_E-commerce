937792789da146fd1ca9fcfc316bf727f2463b9f
Edison Andres Perez Martinez <edisonandresperezmartinez@gmail.com>
2025-09-17 15:20:51 -0500

    colocar la orden, total y subTotal

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,8 @@
 
 import { auth } from '@/auth.config'
 import { Address } from '@/interfaces'
+import { prisma } from '@/lib/prisma'
+import { th } from 'zod/locales'
 
 interface ProductToOrder {
   productId: string
@@ -23,12 +25,50 @@ export const PlaceOrder = async (
     }
   }
 
-  console.log({ productIds, address, userId })
-  
-  // TODO: Implementar lógica para crear la orden
+
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map( p => p.productId)
+      }
+    }
+  })
+ //console.log(products)
+
+
+  const itemsInOrder = productIds.reduce(( count, p ) => count + p.quantity, 0)
+  console.log({ itemsInOrder })
+
+
+
+  const {subTotal, tax, total} = productIds.reduce((totals, item) => {
+
+
+    const productQuantity = item.quantity;
+    const product = products.find(p => p.id === item.productId)
+
+
+      if (!product) throw new Error('Producto no encontrado')
+
+
+        const subTotal = product.msrp * productQuantity;
+
+      totals.subTotal += subTotal;
+      totals.tax += subTotal * 0.15;
+      totals.total += subTotal * 1.15;
+
+  return totals;
+  }, {subTotal: 0, tax: 0, total: 0})
+
+
+console.log ({subTotal, tax, total})
+
+
+
+  //console.log({ productIds, address, userId })
   return {
     ok: true,
     message: 'Orden creada exitosamente',
-    orderId: '12345' // Temporal
+    orderId: '12345' 
   }
 }
